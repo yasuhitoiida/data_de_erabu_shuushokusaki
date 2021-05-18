855f24468e9d173a42eb04f35a0d3a00b25573c8
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-05-19 03:18:37 +0900

    初期設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module TenshokuHandan
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.javascripts false
+      g.stylesheets false
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      g.skip_routes true
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
