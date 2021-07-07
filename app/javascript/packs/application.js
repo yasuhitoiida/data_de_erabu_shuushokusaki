e16b3b447a913d88d81af52a75c26ad9a260bbad
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-07-07 09:40:57 +0900

    デプロイ後のデザイン確認のため

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,35 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import Vue from 'vue'
+import App from '../app.vue'
+import axios from '../plugins/axios.js'
+import router from '../router/index.js'
+import store from '../store/index.js'
+import calculator from '../plugins/calculator.js'
+import chart from '../plugins/chart.js'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+Vue.use(Vuetify)
+const vuetify = new Vuetify();
+Vue.prototype.$axios = axios
+Vue.prototype.$calculator = calculator
+Vue.prototype.$chart = chart
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    render: h => h(App),
+    vuetify,
+    router,
+    store
+  }).$mount()
+  document.body.appendChild(app.$el)
+
+  console.log(app)
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
