c7d450947ec73eab5719b5a40fbf0ca197328f53
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-08-04 18:29:22 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,8 @@ set :repo_url,  'git@github.com:yasuhitoiida/jobhunters-choice.git'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.7.1'
 
