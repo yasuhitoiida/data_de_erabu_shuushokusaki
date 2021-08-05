2fdde823baf39394cb8cfbb7ef8ce11fd66d3777
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-08-05 17:52:45 +0900

    Revert "delete javascript/packs/application.js"
    This reverts commit c4809d2ebfbbd9e8acd8d11ac9c4a56d2e6e5822.

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,45 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+// import Vue from 'vue'
+// import App from '../app.vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import axios from '../plugins/axios.js'
+// import router from '../router/index.js'
+// import store from '../store/index.js'
+// import calculator from '../plugins/calculator.js'
+// import chart from '../plugins/chart.js'
+// import Vuetify from 'vuetify'
+// import 'vuetify/dist/vuetify.min.css'
+// import '@mdi/font/css/materialdesignicons.css'
+// import 'material-design-icons-iconfont/dist/material-design-icons.css'
+//
+// Vue.use(Vuetify)
+// const vuetify = new Vuetify();
+// Vue.prototype.$axios = axios
+// Vue.prototype.$calculator = calculator
+// Vue.prototype.$chart = chart
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   const app = new Vue({
+//     render: h => h(App),
+//     vuetify,
+//     router,
+//     store
+//   }).$mount()
+//   document.body.appendChild(app.$el)
+//
+//   console.log(app)
+// })
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
