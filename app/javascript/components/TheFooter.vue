1779e33456695f82904d5d0c86001961aafeb23d
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-08-06 21:14:43 +0900

    #31 利用規約とプライバシーポリシー

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -1,16 +1,29 @@
 <template>
   <v-footer>
-    <v-col align="center">
-      © 2021 JobHunter's Choice
+    <v-col align="center" class="p-1">
+      <v-btn
+        to='/user_policy'
+        plain
+      >USER POLICY</v-btn>
+      <v-btn
+        to='/privacy_policy'
+        plain
+      >PRIVACY POLICY</v-btn>
+      <v-btn
+        href="mailto:jobhunterschoice@gmail.com"
+        plain
+        >
+        CONTACT US
+      </v-btn>
+      <div>
+        © 2021 JobHunter's Choice
+      </div>
     </v-col>
   </v-footer>
 </template>
 
-<script>
-import { mapGetters } from 'vuex'
-export default {
-}
-</script>
-
 <style scoped>
+div {
+  font-size: 4px;
+}
 </style>
