f720613af4c7de1103bd9dcdc74407415e2fa314
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-08-12 13:32:59 +0900

    引用文献の明記

--- app/javascript/components/AboutAHP.vue
+++ app/javascript/components/AboutAHP.vue
@@ -4,7 +4,7 @@
     <p>
       AHP（階層分析法）は複雑な状況での意思決定を行うための構造化法の1つである。AHPは個人的な決定問題で大いに役立つが、複雑な決定問題に取り組む集団意思決定場面で大きな効果を発揮する。例えば利害関係がある場合、人間の認知や判断に頼らざるを得ない場合、さらにはその結果が長期にわたり影響を与える場合に大きな成果をあげている。
     </p>
-    <b>AHPの活用場面</b>
+    <b>AHPの活用場面<sup>1</sup></b>
     <ul>
       <li>
         選択 － 複数の代替案の中から1つの代替案を選択する。
@@ -28,22 +28,22 @@
     <b>AHPの活用例</b>
     <ul>
       <li>
-        世界的な気候変動の影響を減らす最良の方法は何か（Fondazione Eni Enrico Mattei）
+        世界的な気候変動の影響を減らす最良の方法は何か（Fondazione Eni Enrico Mattei）<sup>2</sup>
       </li>
       <li>
-        ソフトウェアシステムの総合品質の数量化（マイクロソフト株式会社）
+        ソフトウェアシステムの総合品質の数量化（マイクロソフト株式会社）<sup>3</sup>
       </li>
       <li>
-        大学教授の選抜（Bloomsburg University of Pennsylvania）
+        大学教授の選抜（Bloomsburg University of Pennsylvania）<sup>4</sup>
       </li>
       <li>
-        海外製造工場の建設場所の選定（ケンブリッジ大学）
+        海外製造工場の建設場所の選定（ケンブリッジ大学）<sup>5</sup>
       </li>
       <li>
-        全国を横断する石油パイプライン運用のリスク査定（アメリカ土木学会）
+        全国を横断する石油パイプライン運用のリスク査定（アメリカ土木学会）<sup>6</sup>
       </li>
       <li>
-        アメリカ合衆国の分水点に関する最良なマネジメント法の策定（アメリカ農務省）
+        アメリカ合衆国の分水点に関する最良なマネジメント法の策定（アメリカ農務省）<sup>7</sup>
       </li>
     </ul>
     <b>AHPの手順</b>
@@ -62,6 +62,16 @@
       </li>
     </ol>
     <img src="../../assets/images/1247472500567252cb2c0370609f634c.png">
+    <b>引用文献</b>
+    <ol>
+      <li>Forman, Ernest H.; Saul I. Gass (2001-07). “The Analytical Hierarchy Process—An Exposition”. Operations Research 49 (4): 469–487. </li>
+      <li>Berrittella, M.; A. Certa, M. Enea, P. Zito (January, 2007). “An Analytic Hierarchy Process for the Evaluation of Transport Policies to Reduce Climate Change Impacts”.</li>
+      <li>McCaffrey, James (June 2005). “Test Run: The Analytic Hierarchy Process”.</li>
+      <li>Grandzol, John R. (August 2005). “Improving the Faculty Selection Process in Higher Education: A Case for the Analytic Hierarchy Process”.</li>
+      <li>Atthirawong, Walailak; Bart McCarthy (September, 2002). “An Application of the Analytical Hierarchy Process to International Location Decision-Making”.</li>
+      <li>Dey, Prasanta Kumar (November 2003). “Analytic Hierarchy Process Analyzes Risk of Operating Cross-Country Petroleum Pipelines in India”. Natural Hazards Review 4 (4): 213–221. </li>
+      <li>de Steiguer, J.E.; Jennifer Duberstein, Vicente Lopes (October 2003). “The Analytic Hierarchy Process as a Means for Integrated Watershed Management”.</li>
+    </ol>
   </div>
 </template>
 
