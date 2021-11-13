43a4dd5fc6849fe29384fed77cfb5cbe97472308
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-11-14 05:20:42 +0900

    Fix:babel.config

--- babel.config.js
+++ babel.config.js
@@ -72,6 +72,12 @@ module.exports = function(api) {
         {
           async: false
         }
+      ],
+      [
+        "@babel/plugin-proposal-private-property-in-object",
+        {
+          loose: true 
+        }
       ]
     ].filter(Boolean)
   }
